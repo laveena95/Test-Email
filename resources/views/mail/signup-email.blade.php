33c4d9865713ccbfa70216da1d44740b072e20a3
laveena95 <laveenalavee256@gmail.com>
2020-06-19 16:43:49 +0530

    Email Verification

---
+++ resources/views/mail/signup-email.blade.php
@@ -0,0 +1,11 @@
+Hello {{$email_data['name']}}!,
+<br><br>
+Welcome to Helping Hands!
+<br>
+Please click the below link to verify your email and activate your account!
+<br><br>
+<a href="http://localhost/my_tuts/send-emails/blog/public/verify?code={{$email_data['verification_code']}}">Click Here>>></a>
+
+<br><br>
+Thank You!
+<br>
